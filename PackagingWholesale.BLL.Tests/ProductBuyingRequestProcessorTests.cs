e5d042802bb61d61709afab4ad7fa95a3d19fa9f
Jarek Bigaj <j.bigaj@interia.pl>
2021-03-26 11:42:24 +0100

    feat: implement simple business logic Product Buying

---
+++ PackagingWholesale.BLL.Tests/ProductBuyingRequestProcessorTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Xunit;
+using PackagingWholesale.BLL.Products.Buying;
+
+
+namespace PackagingWholesale.BLL.Tests
+{
+    public class ProductBuyingRequestProcessorTests
+    {
+        private ProductBuyingRequestProcessor _processor;
+        private ProductBuyingRequest _request;
+        public ProductBuyingRequestProcessorTests()
+        {
+            _processor = new ProductBuyingRequestProcessor();
+
+            //Arrange
+            _request = new ProductBuyingRequest()
+            {
+                FirstName = "Jarek",
+                LastName = "Bigaj",
+                Email = "jarek@example.com",
+                Date = DateTime.Now
+            };
+        }
+
+        [Fact]
+        public void ShouldReturnProductBuyingResultWithRequestValues()
+        {
+
+            //Act
+            ProductBuyingResult result = _processor.BuyProduct(_request);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(_request.FirstName, result.FirstName);
+            Assert.Equal(_request.LastName, result.LastName);
+            Assert.Equal(_request.Email, result.Email);
+            Assert.Equal(_request.Date, result.Date);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionIfResultIsNull()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(
+                    () => _processor.BuyProduct(null)
+            );
+
+            Assert.Equal("request", exception.ParamName);
+        }
+
+        [Fact]
+        public void ShouldReturnStatusTrueWhenSendedCorrectValues()
+        {
+            //Act
+            ProductBuyingResult result = _processor.BuyProduct(_request);
+
+            //Assert
+            Assert.Equal(true, result.IsStatusOk);
+            Assert.Equal(0, result.Errors.Count);
+
+        }
+    }
+}
---
+++ PackagingWholesale.BLL/Products/Buying/ProductBuyingRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PackagingWholesale.BLL.Products.Buying
+{
+    public class ProductBuyingRequest
+    {
+        public ProductBuyingRequest()
+        {
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
